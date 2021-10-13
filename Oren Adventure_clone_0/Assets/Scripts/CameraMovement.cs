6c200195b85a4f0dc6c7540dd0345bbe648f4277
Tuoof <72484719+Tuoof@users.noreply.github.com>
2021-10-13 10:47:35 +0700

    Update Input System
    Change to Unity New Input System

---
+++ Oren Adventure_clone_0/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float stiffness;
+    public Transform player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        float t = Time.deltaTime * stiffness;
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, player.position.x, t), Mathf.Lerp(transform.position.y, player.position.y, t), transform.position.z);
+    }
+
+    public void TeleportTo(Vector2 _transform)
+    {
+        transform.position = new Vector3(_transform.x, _transform.y, transform.position.z);
+    }
+}
