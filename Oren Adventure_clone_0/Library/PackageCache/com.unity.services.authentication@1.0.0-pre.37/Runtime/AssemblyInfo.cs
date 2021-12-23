4a2d5e0b44a1b10d0c2040145606496b01eecb57
Tuoof <72484719+Tuoof@users.noreply.github.com>
2021-12-24 05:13:32 +0700

    Revert "Features"

--- Oren Adventure_clone_0/Library/PackageCache/com.unity.services.authentication@1.0.0-pre.37/Runtime/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Services.Authentication.Editor")]
-[assembly: InternalsVisibleTo("Unity.Services.Authentication.Tests")]
-[assembly: InternalsVisibleTo("Unity.Services.Authentication.EditorTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // For Moq
